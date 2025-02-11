04a265486e1d6c4ac7014040428658e0098cb198
Keerthana P B <166291742+kethkee@users.noreply.github.com>
2025-02-11 22:53:36 +0530

    Create RR temp.c

---
+++ RR temp.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  int i,limit,total=0,x,counter=0,time_quantum;
+  int wt=0,tt=0,bt[10],temp[10];
+  float avgwt,avgtt;
+  printf("\nEnter total number of processes:\t");
+  scanf("%d",&limit);
+  x=limit;
+  for(i=0;i<limit;i++)
+  {
+      
+  
+  
